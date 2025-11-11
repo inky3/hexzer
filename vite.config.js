25caaef30233e02b672321d1116956234a323850
unknown <Boom05@windowslive.com>
2025-11-11 21:57:03 +0700

    branch update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hexzer/", // ต้องตรงกับชื่อ repo
+  base: "/hexzer/",   // repo name; for username.github.io leave as "/"
 });
+  
