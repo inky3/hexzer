85c0c755336a21ded7a74705e12be5ca37be42da
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-11-13 16:31:08 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // repo name; for username.github.io leave as "/"
+  base: "/hexzer/",   // repo name; for username.github.io leave as "/"
 });
   
